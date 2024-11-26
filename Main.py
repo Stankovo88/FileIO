137f3bfb7897592bc52af43d00a4dcc84d228e54
Alex <stankovo88@gmail.com>
2024-11-26 20:53:29 +0300

    Добавлена функция загрузки данных из файла в формате JSON и отображения данных в 2х списках

--- Main.py
+++ Main.py
@@ -21,7 +21,6 @@ def save_history(file_path, link):
         json.dump(history, f, indent=4)
 
 
-
 def upload():
     try:
         filepath = fd.askopenfilename()
@@ -41,7 +40,25 @@ def upload():
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
 
+def show_history():
+    if not os.path.exists(history_file):
+        mb.showinfo("История", "История загрузок пуста")
+        return
 
+    history_window = Toplevel(window)
+    history_window.title("История загрузок")
+
+    files_listbox = Listbox(history_window, width=50,height=20)
+    files_listbox.grid(row=0,column=0,padx=(10,0),pady=10)
+
+    links_listbox = Listbox(history_window, width=50, height=20)
+    links_listbox.grid(row=0, column=1, padx=(0, 10), pady=10)
+
+    with open(history_file, "r") as f:
+        history = json.load(f)
+        for item for history:
+            files_listbox.insert(END, item['file_path'])
+            links_listbox.insert(END, item['download_link'])
 
 
 
@@ -55,5 +72,8 @@ button.pack()
 
 entry = ttk.Entry()
 entry.pack()
+
+history_button =ttk.Button("Показать историю",command=show_history)
+history_button.pack()
 
 window.mainloop()
